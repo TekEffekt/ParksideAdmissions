ec3ccb25bd200f7cb14a6743fdbe7132f1cd7360
Kyle K Zawacki <z95sk8@outlook.com>
2015-09-13 17:35:40 -0500

    Initial Commit

---
+++ Parkside Admissions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parkside Admissions
+//
+//  Created by Kyle Zawacki on 9/13/15.
+//  Copyright © 2015 University Of Wisconsin Parkside. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Parkside AdmissionsUITests/Parkside_AdmissionsUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Parkside_AdmissionsUITests.swift
+//  Parkside AdmissionsUITests
+//
+//  Created by Kyle Zawacki on 9/13/15.
+//  Copyright © 2015 University Of Wisconsin Parkside. All rights reserved.
+//
+
+import XCTest
+
+class Parkside_AdmissionsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
